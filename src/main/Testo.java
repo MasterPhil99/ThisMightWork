5e840c552471b4757aa9e9f15f7841d96570c96a
Philipp Maurer <30367319+MasterPhil99@users.noreply.github.com>
2019-07-21 22:09:19 +0200

    Revert "Featurenumerouno"

--- src/main/Testo.java
+++ src/main/Testo.java
@@ -6,14 +6,13 @@ import javax.swing.*;
 
 public class Testo {
     public static void main(String[] args) {
-        int frameWidth = 600, frameHeight = 600;
-        DrawSpace ds = new DrawSpace(new Double(frameWidth), new Double(frameHeight));
+        int frameWidth = 1000, frameHeight = 500;
+        DrawSpace ds = new DrawSpace(frameWidth, frameHeight);
         JFrame frame = new JFrame();
         frame.add(ds);
+        frame.setVisible(true);
         frame.setSize(frameWidth, frameHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Crazy shit yo");
-        frame.setResizable(false);
-        frame.setVisible(true);
     }
 }
--- src/view/DrawSpace.java
+++ src/view/DrawSpace.java
@@ -1,9 +1,5 @@
 package view;
 
-import model.GameObject;
-import model.Goal;
-import model.Player;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -14,24 +10,20 @@ import java.awt.geom.Rectangle2D;
 
 public class DrawSpace extends JPanel implements ActionListener, KeyListener {
     Timer timer = new Timer(5, this);
-    Player player;
-    Goal goal;
-    double frameWidth;
-    double frameHeight;
 
+    double xPosition = 1;
+    double yPosition = 1;
+    double xVelocity = 0;
+    double yVelocity = 0;
+    double width = 10;
+    double height = 10;
+    int frameWidth;
+    int frameHeight;
 
-    public DrawSpace(double frameWidth, double frameHeight) {
+    public DrawSpace(int frameWidth, int frameHeight) {
+        timer.start();
         this.frameWidth = frameWidth;
         this.frameHeight = frameHeight;
-        reset();
-    }
-
-    public void reset() {
-        this.player = new Player(1,1,0, 0,10,10);
-        this.goal = new Goal(this.frameWidth * 0.8, this.frameHeight * 0.8, 0, 0, 30, 30);
-        this.player.setColor(Color.BLACK);
-        this.goal.setColor(Color.GREEN);
-        this.timer.start();
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -40,70 +32,61 @@ public class DrawSpace extends JPanel implements ActionListener, KeyListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        Rectangle2D pl = new Rectangle2D.Double(player.getxPosition(), player.getyPosition(), player.getWidth(), player.getHeight());
-        Rectangle2D go = new Rectangle2D.Double(goal.getxPosition(), goal.getyPosition(), goal.getWidth(), goal.getHeight());
-        g2.setColor(player.getColor());
-        g2.fill(pl);
-        g2.setColor(goal.getColor());
-        g2.fill(go);
+        Rectangle2D circle = new Rectangle2D.Double(xPosition, yPosition, width, height);
+        g2.fill(circle);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (keepXInBounds()) {
-            player.setxPosition(player.getxPosition() + player.getxVelocity());
+            this.xPosition += xVelocity;
         }
         if (keepYInBounds()) {
-            player.setyPosition(player.getyPosition() + player.getyVelocity());
-        }
-        if (player.isCollision(goal)) {
-            this.timer.stop();
-            JOptionPane.showMessageDialog(this, "Congrats, you beat the level!");
-            reset();
+            this.yPosition += yVelocity;
         }
         repaint();
     }
 
     private boolean keepYInBounds() {
-        if (player.getyPosition() + player.getyVelocity() < 0 || player.getyPosition() + player.getyVelocity() > this.frameHeight - (player.getHeight() + 38)) return false;
+        if (this.yPosition + this.yVelocity < 0 || this.yPosition + this.yVelocity > this.frameHeight - (this.height + 38)) return false;
         else return true;
     }
 
     private boolean keepXInBounds() {
-        if (player.getxPosition() + player.getxVelocity() < 0 || player.getxPosition() + player.getxVelocity() > this.frameWidth - (player.getWidth() + 14)) return false;
+        if (this.xPosition + this.xVelocity < 0 || this.xPosition + this.xVelocity > this.frameWidth - (this.width + 14)) return false;
         else return true;
     }
 
     public void upPressed() {
-        player.setyVelocity(-1.5);
+        this.yVelocity = -1.5;
     }
 
     public void downPressed() {
-        player.setyVelocity(1.5);
+        this.yVelocity = 1.5;
     }
 
     public void leftPressed() {
-        player.setxVelocity(-1.5);
+        this.xVelocity = -1.5;
     }
 
     public void rightPressed() {
-        player.setxVelocity(1.5);
+        this.xVelocity = 1.5;
     }
 
     public void upReleased() {
-        if (player.getyVelocity() < 0) player.setyVelocity(0);
+        if (this.yVelocity < 0) this.yVelocity = 0;
     }
 
     public void downReleased() {
-        if (player.getyVelocity() > 0) player.setyVelocity(0);
+        if (this.yVelocity > 0) this.yVelocity = 0;
     }
 
     public void leftReleased() {
-        if (player.getxVelocity() < 0) player.setxVelocity(0);
+        if (this.xVelocity < 0) this.xVelocity = 0;
     }
 
     public void rightReleased() {
-        if (player.getxVelocity() > 0) player.setxVelocity(0);
+        if (this.xVelocity > 0) this.xVelocity = 0;
     }
     @Override
     public void keyTyped(KeyEvent e) {
