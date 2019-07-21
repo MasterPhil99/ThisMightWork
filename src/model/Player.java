8b9245509617a800276c97deaa63e15e09fdda65
Philipp Maurer <agaagjaan@gmail.com>
2019-07-21 23:48:02 +0200

    Obstacles are now in a collection to dynamically load them onto the screen

--- src/model/Player.java
+++ src/model/Player.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Player extends GameObject {
     public Player(double xPosition, double yPosition, double xVelocity, double yVelocity, double width, double height) {
         super(xPosition, yPosition, xVelocity, yVelocity, width, height);
-        super.setColor(Color.BLACK);
+        super.setColor(Color.DARK_GRAY);
     }
 
 }
